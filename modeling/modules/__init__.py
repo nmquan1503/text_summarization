6ba57bddbb9bd93d5aa06cba0fc18d56ac8f5495
nmquan1503 <minhquan15032005@gmail.com>
2026-04-25 13:46:55 +0700

    feat: implement block mamba 2

---
+++ modeling/modules/__init__.py
@@ -0,0 +1,2 @@
+from modeling.modules.block import Block
+from modeling.modules.rms_norm import RMSNorm
---
+++ modeling/modules/block.py
@@ -0,0 +1,191 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from typing import Tuple
+
+from modeling.modules.rms_norm import RMSNorm
+from modeling.ops.ssd_scan import SSDScanFn
+
+class Block(nn.Module):
+    def __init__(
+        self,
+        model_dim: int = 512,
+        state_dim: int = 128,
+        conv_kernel: int = 4,
+        head_dim: int = 64,
+        num_groups: int = 1,
+        expansion_factor: int = 2,
+        chunk_size: int = 256,
+        delta_limit: Tuple[float, float] = (0.0, float("inf")),
+        A_init_range: Tuple[int, int] = (1, 16),
+        delta_init_limit: Tuple[float, float] = (0.001, 0.1),
+        delta_init_floor: float = 1e-4,
+        dropout_rate: float = 0.15,
+        device="cuda"
+    ):
+        super().__init__()
+
+        self.model_dim = model_dim
+        self.state_dim = state_dim
+        self.inner_dim = expansion_factor * model_dim
+        assert self.inner_dim % head_dim == 0
+        self.conv_kernel = conv_kernel
+        self.head_dim = head_dim
+        self.num_heads = self.inner_dim // head_dim
+        self.num_groups = num_groups
+        assert self.num_heads % num_groups == 0
+        self.chunk_size = chunk_size
+        self.delta_limit = delta_limit
+
+        self.norm = RMSNorm(self.model_dim)
+
+        self.in_proj = nn.Linear(self.model_dim, 2 * self.inner_dim + 2 * self.num_groups * self.state_dim + self.num_heads)
+
+        conv_dim = self.inner_dim + 2 * self.num_groups * self.state_dim
+        self.conv = nn.Conv1d(
+            in_channels=conv_dim,
+            out_channels=conv_dim,
+            kernel_size=conv_kernel,
+            groups=conv_dim,
+            padding=conv_kernel - 1
+        )
+
+        delta_init = torch.exp(
+            torch.rand(self.num_heads, device=device, dtype=torch.float32) 
+            * (math.log(delta_init_limit[1]) - math.log(delta_init_limit[0]))
+            + math.log(delta_init_limit[0])
+        )
+        delta_init = torch.clamp(delta_init, min=delta_init_floor)
+        inv_delta_init = delta_init + torch.log(-torch.expm1(-delta_init))
+        self.delta_bias = nn.Parameter(inv_delta_init)
+        self.delta_bias._no_weight_decay = True
+    
+        A = torch.empty(self.num_heads, dtype=torch.float32, device=device).uniform_(*A_init_range)
+        A_log = torch.log(A)
+        self.A_log = nn.Parameter(A_log)
+        self.A_log._no_weight_decay = True
+
+        self.D = nn.Parameter(torch.ones(self.inner_dim, device=device))
+        self.D._no_weight_decay = True
+
+        self.out_proj = nn.Linear(self.inner_dim, self.model_dim)
+    
+        self.dropout = nn.Dropout(dropout_rate)
+
+        self._ssm_hiddens = None
+        # (batch_size, conv_dim, conv_kernel - 1)
+        self._conv_context = None
+
+    def forward(
+        self, 
+        hidden_states: torch.Tensor, 
+        lengths: torch.Tensor | None = None,
+        ssm_hiddens: torch.Tensor | None = None, 
+        conv_context: torch.Tensor | None = None,
+        use_cache: bool = False
+    ):
+        """
+        Args:
+            hidden_states: (batch_size, seq_len, model_dim)
+            lengths: (batch_size,)
+            ssm_hiddens: (batch_size, inner_dim, state_dim)
+            conv_context: (batch_size, hBC_dim, conv_kernel - 1)
+        
+        Returns: 
+            hidden_states: (batch_size, seq_len, model_dim)
+            last_ssm_hiddens: (batch_size, inner_dim, state_dim)
+        """
+        batch_size, seq_len, _ = hidden_states.shape
+        device = hidden_states.device
+        has_conv_context = conv_context is not None
+
+        block_residual = hidden_states
+        
+        hidden_states = self.norm(hidden_states)
+
+        hBC, gate_logits, delta_raw = torch.split(
+            self.in_proj(hidden_states),
+            [
+                self.inner_dim + 2 * self.num_groups * self.state_dim,
+                self.inner_dim,
+                self.num_heads
+            ],
+            dim=-1
+        )
+
+        A = -torch.exp(self.A_log)
+
+        hBC = hBC.transpose(1, 2)
+
+        if has_conv_context:
+            hBC = torch.cat([conv_context, hBC], dim=2)
+
+        if use_cache:
+            # Cache conv context
+            cache_size = self.conv_kernel - 1
+            total_len = hBC.size(2)
+            if total_len >= cache_size:
+                cached_ctx = hBC[:, :, -cache_size:].detach()
+            else:
+                pad_left = cache_size - total_len
+                cached_ctx = F.pad(hBC, (pad_left, 0), value=0).detach()
+            self._conv_context = cached_ctx
+        
+        hBC = F.silu(hBC)
+        hBC = self.conv(hBC).transpose(1, 2)
+
+        if has_conv_context:
+            hBC = hBC[:, self.conv_kernel - 1 : self.conv_kernel - 1 + seq_len]
+        else:
+            hBC = hBC[:, :seq_len]
+
+        hidden_states, B, C = torch.split(
+            hBC,
+            [
+                self.inner_dim,
+                self.num_groups * self.state_dim,
+                self.num_groups * self.state_dim
+            ],
+            dim=-1
+        )
+
+        B = B.view(batch_size, seq_len, self.num_groups, self.state_dim)
+        C = C.view(batch_size, seq_len, self.num_groups, self.state_dim)
+
+        ssm_residual = hidden_states
+        hidden_states, last_ssm_hiddens = SSDScanFn.apply(
+            hidden_states, A, B, C, delta_raw, self.delta_bias, ssm_hiddens, lengths,
+            chunk_size=self.chunk_size, delta_limit=self.delta_limit
+        )
+
+        if use_cache:
+            self._ssm_hiddens = last_ssm_hiddens
+
+        hidden_states = hidden_states + self.D * ssm_residual
+
+        hidden_states = hidden_states * F.silu(gate_logits)
+
+        hidden_states = self.out_proj(hidden_states)
+
+        hidden_states = self.dropout(hidden_states) + block_residual
+
+        return hidden_states, last_ssm_hiddens
+
+    def step(self, hidden_states: torch.Tensor):
+        """
+        Args: (batch_size, model_dim)
+        Returns: (batch_size, model_dim)
+        """
+        hidden_states = hidden_states.unsqueeze(1)
+        chunk_size = self.chunk_size
+        self.chunk_size = 1
+        hidden_states, _ = self.forward(
+            hidden_states=hidden_states,
+            ssm_hiddens=self._ssm_hiddens,
+            conv_context=self._conv_context,
+            use_cache=True
+        )
+        self.chunk_size = chunk_size
+
+        return hidden_states.squeeze(1)
