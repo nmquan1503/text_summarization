4955a74269f0e4656df20ce14ec33290b97b6823
nmquan1503 <minhquan15032005@gmail.com>
2026-04-26 11:47:28 +0700

    feat: implement sentencepiece tokenizer

---
+++ config.py
@@ -0,0 +1,8 @@
+# Dataset config
+TRAIN_PATH = ""
+DEV_PATH = ""
+TEST_PATH = ""
+
+# Tokenizer config
+VOCAB_SIZE = 32000
+SPM_MODEL_PATH = ""
---
+++ data/tokenizer.py
@@ -0,0 +1,63 @@
+import sentencepiece as spm
+from pathlib import Path
+from typing import List
+import pandas as pd
+import os
+
+import config
+
+def train_tokenizer():
+    df = pd.read_csv(config.TRAIN_PATH)
+    texts = list(df["source"]) + list(df["target"])
+    
+    with open("_temp_texts.txt", "w", encoding="utf-8") as f:
+        for t in texts:
+            f.write(t.strip() + "\n")
+    
+    spm.SentencePieceTrainer.Train(
+        input=f"_temp_texts.txt",
+        model_prefix=config.SPM_MODEL_PATH.split(".model")[0],
+        vocab_size=config.VOCAB_SIZE,
+        model_type="unigram",
+        character_coverage=0.9995,
+        pad_id=0,
+        unk_id=1,
+        bos_id=2,
+        eos_id=3,
+        minloglevel=2
+    )
+
+    os.remove("_temp_texts.txt")
+
+class Tokenizer:
+    def __init__(self):
+        if not Path(config.SPM_MODEL_PATH).exists():
+            train_tokenizer()
+        
+        self.sp = spm.SentencePieceProcessor()
+        self.sp.Load(config.SPM_MODEL_PATH)
+
+        self.pad_id = self.sp.pad_id()
+        self.unk_id = self.sp.unk_id()
+        self.bos_id = self.sp.bos_id()
+        self.eos_id = self.sp.eos_id()
+    
+    def encode(self, texts: str | List[str], add_bos=True, add_eos=True):
+        ids = self.sp.Encode(texts, out_type=int)
+
+        if add_bos:
+            if isinstance(texts, str):
+                ids = [self.bos_id] + ids
+            else:
+                ids = [[self.bos_id] + i for i in ids]
+        
+        if add_eos:
+            if isinstance(texts, str):
+                ids = ids + [self.eos_id]
+            else:
+                ids = [i + [self.eos_id] for i in ids]
+        
+        return ids
+
+    def decode(self, ids: List[int] | List[List[int]]):
+        return self.sp.Decode(ids)
