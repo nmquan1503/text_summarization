d9e44e3b5d296abb1ae865fb1bdd97d91279513c
nmquan1503 <minhquan15032005@gmail.com>
2026-04-26 22:08:37 +0700

    fix: mismatch type

--- data/dataset.py
+++ data/dataset.py
@@ -21,7 +21,7 @@ class CausalLMDataset(Dataset):
         return {
             "input_ids": torch.tensor(src + tgt[:-1], dtype=torch.long),
             "labels": torch.tensor([self.tokenizer.pad_id] * len(src) + tgt[1:], dtype=torch.long),
-            "gen_input_ids": torch.tensor(src + tgt[0], dtype=torch.long),
+            "gen_input_ids": torch.tensor(src + tgt[:1], dtype=torch.long),
             "terget_ids": tgt
         }
 
