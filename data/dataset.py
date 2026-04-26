054080368db2a79325b9fadcc9c6536807432a0e
nmquan1503 <minhquan15032005@gmail.com>
2026-04-26 11:56:03 +0700

    feat: implement dataset for seq2seq model

---
+++ data/dataset.py
@@ -0,0 +1,21 @@
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+
+from data.tokenizer import Tokenizer
+
+class Seq2SeqDataset(Dataset):
+    def __init__(self, dataset_path: str, tokenizer: Tokenizer):
+        df = pd.read_csv(dataset_path)
+
+        self.src_ids = tokenizer.encode(list(df["source"]), add_bos=False, add_eos=False)
+        self.tgt_ids = tokenizer.encode(list(df["target"]), add_bos=True, add_eos=True)
+
+    def __len__(self):
+        return len(self.src_ids)
+
+    def __getitem__(self, index):
+        return {
+            "input_ids": torch.tensor(self.src_ids[index], dtype=torch.long),
+            "target_ids": torch.tensor(self.tgt_ids[index], dtype=torch.long)
+        }
